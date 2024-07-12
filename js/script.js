1558f3fcbae8136928ab169ca2e5809e6eaea576
eltonanit <elton.ani@gmail.com>
2024-07-12 09:50:13 +0200

    [UPD] Genero 100 celle e la griglia

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+// Recupero la griglia
+document.getElementById('generateGrid').addEventListener('click', function () {
+    
+    const grid = document.getElementById('grid');
+    grid.innerHTML = '';
+
+    // Genero le celle
+    for (let i = 1; i <= 100; i++) {
+        const square = document.createElement('div');
+        square.className = 'square';
+        square.textContent = i;
 
 
-btn
+});
